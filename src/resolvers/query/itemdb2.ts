ebeb8c820ced271ab2f01901ff668a4c3bbf438d
Francisco Javier Guerrero Martinez <diwolf@live.de>
2021-07-22 19:02:22 -0500

    Update form query items weapon

--- src/resolvers/query/itemdb2.ts
+++ src/resolvers/query/itemdb2.ts
@@ -1,3 +1,4 @@
+import { ITEMDB2_LIST_WEAPONS } from "./../../sql/SQL_ITEM_DB";
 import { ITEMDB2_LIST } from "../../sql/SQL_ITEM_DB";
 import { IResolvers } from "graphql-tools";
 
@@ -9,6 +10,19 @@ const resolverQueryItemDb2: IResolvers = {
       const item = Array(0);
       try {
         const [rows]: any[] = await sql.execute(sentencia);
+        return rows;
+      } catch (error: any) {
+        throw new Error(error);
+      }
+    },
+
+    //OBTENER ITEMS ARMAS
+    async getItemWeapon(_, { type, view }, { sql }) {
+      const sentencia = ITEMDB2_LIST_WEAPONS;
+
+      const item = Array(0);
+      try {
+        const [rows]: [any] = await sql.execute(sentencia, [type, view]);
         return rows;
       } catch (error: any) {
         throw new Error(error);
--- src/sql/SQL_ITEM_DB.ts
+++ src/sql/SQL_ITEM_DB.ts
@@ -1,2 +1,4 @@
 export const ITEMDB2_LIST =
   "SELECT item_db2.id, item_db2.name_japanese, renewal.descripcion, renewal.imagen, item_db2.type, item_db2.view FROM item_db2, renewal WHERE item_db2.id = renewal.idNueva";
+export const ITEMDB2_LIST_WEAPONS =
+  "SELECT item_db2.id,item_db2.name_english,item_db2.type,item_db2. VIEW AS class,item_db2.price_buy,item_db2.price_sell,item_db2.attack,item_db2.equip_level,item_db2.weapon_level,item_db2.slots,item_db2.refineable,renewal.descripcion,renewal.imagen FROM item_db2,renewal WHERE item_db2.type = ? AND item_db2.id = renewal.idNueva AND view = ? ORDER BY id DESC";
